6537defa5100f9bf0ea597dd7b0667e4823354b4
Boyd smit <boydsmit@live.nl>
2017-03-29 21:02:54 +0200

    init

---
+++ views/about.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Boyd smit
+ * Date: 3/8/2017
+ * Time: 15:04
+ */
---
+++ views/head.php
@@ -0,0 +1,17 @@
+<?php
+
+// Hier vind je voorbeeld html voor de head-sectie
+
+?><!DOCTYPE html>
+
+<meta charset="utf-8">
+<head>
+	<title><?php echo $title;?></title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width">
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+	<link type="text/css" rel="stylesheet" href="css/main.css">
+</head>
+<body>
